cfa3b59a07a42d631123570f3673faee9ded3fe0
Luffy <luffy014@example.com>
2025-10-08 10:09:13 +0000

    testing

--- code.sh
+++ code.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "hi there"
-echo "This is my first code"
-echo "Let see how this goes"
+echo "Enter your first name and last name"
+read first 
+read last
+echo "what is your first name: $first"
+echo "what is you last name: $last"
+echo "Hi $first $last"
+read -p "Enter your city you live in ?" city
+echo "You live in $city."
 
